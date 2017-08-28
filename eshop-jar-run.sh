710a8c2d642c21a0bad4f4c245871cb431c6522c
chenfengli1985 <chenfengli1985@126.com>
2017-08-28 15:51:04 +0800

    Update eshop-jar-run.sh

--- eshop-jar-run.sh
+++ eshop-jar-run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-java -jar /jar/auth/eshop_k8s_auth_service.jar --spring.config.location=/jar/auth/application.yml &
+java -jar /jar/eshop_k8s_auth_service.jar --spring.config.location=/jar/application-auth.yml &
 sleep 30s 
-java -jar /jar/cart/eshop_k8s_cart-service.jar --spring.config.location=/jar/cart/application.yml &
+java -jar /jar/eshop_k8s_cart-service.jar --spring.config.location=/jar/application-cart.yml &
 sleep 30s
-java -jar /jar/product/eshop_k8s_product-service.jar --spring.config.location=/jar/product/application.yml &
+java -jar /jar/eshop_k8s_product-service.jar --spring.config.location=/jar/application-product.yml &
 sleep 30s
-java -jar /jar/web/eshop_k8s_eshop_web.jar --spring.config.location=/jar/web/application.yml
+java -jar /jar/eshop_k8s_eshop_web.jar --spring.config.location=/jar/application-web.yml
